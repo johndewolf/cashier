a656c5527bbe6a36538b00f9ad42b7c0581981eb
johndewolf <jdewolf06@gmail.com>
2013-11-15 11:10:55 -0500

    condensed code

--- cashier_calculate.rb
+++ cashier_calculate.rb
@@ -1,27 +1,23 @@
 def valid?(num)
-  !!num.match(/\A\d+(\.\d{2})*\z/)
+  if !num.match(/\A\d+(\.\d{2})*\z/)
+    puts "Warning invalid number"
+  end
 end
 
 puts "What amount is due?"
 amount_due = gets.chomp
+valid?(amount_due)
 
+puts "What is the amount tendered?"
+amount_tend = gets.chomp
+valid?(amount_tend)
 
-if valid?(amount_due) == false
-  puts "Warning invalid number"
-else
-  puts "What is the amount tendered?"
-  amount_tend = gets.chomp
-  if valid?(amount_tend) == false
-    puts "Warning invalid number"
-  else
-    change_due = amount_tend.to_f - amount_due.to_f
-      if change_due > 0
-        puts "Your change is: $#{"%.2f" % change_due}"
-        puts "Time of transaction: #{Time.now}"
-      elsif change_due == 0
-        puts "No change due"
-      else change_due < 0
-        puts "Warning: Customer still owes #{change_due.abs}!"
-      end
-  end
+change_due = amount_tend.to_f - amount_due.to_f
+if change_due > 0
+  puts "Your change is: $#{"%.2f" % change_due}"
+  puts "Time of transaction: #{Time.now}"
+elsif change_due == 0
+  puts "No change due"
+else change_due < 0
+  puts "Warning: Customer still owes #{change_due.abs}!"
 end
